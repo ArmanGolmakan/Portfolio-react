f30c7c4f814d70e90450d1a7acec9f1188346509
Arman <arman_g75@yahoo.com>
2025-01-11 23:58:39 -0500

    updated year

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Arman Golmakan. <br />
+        &copy; 2025 Arman Golmakan. <br />
         All rights reserved.
       </p>
     </section>
